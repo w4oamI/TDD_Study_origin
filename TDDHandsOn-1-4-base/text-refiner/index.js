3eb78f3cf37d50a487356b0b4423f790fd9e45c4
w4oamI <ppas1515@naver.com>
2021-05-08 00:51:59 +0900

    for of문법 및 사용법 확인(처음보는 for문..)

--- TDDHandsOn-1-4-base/text-refiner/index.js
+++ TDDHandsOn-1-4-base/text-refiner/index.js
@@ -26,3 +26,12 @@ module.exports = refineText;
 //하지만 test를 실행해보면 위의 3~9번째 테스트로 성공했던 케이스가 모두 실패한다.
 //11번째 코드를 추가하여 options가 지정되어 있을때만, 실행 하도록 한다면, 모두 성공한다.
 //if에 options만 썼지만, bannedWords도 검사를 해야 더욱 안전한 코드가 된다.
+
+
+
+//13줄 for문 설명
+//javascript에는 for문, for in문, for of문이 존재한다.
+//for of문은 반복할 수 있는 객체를 순회할 수 있도록 해주는 반복문이다.
+//그럼 반복할 수 있는 객체는 무엇이 있을까? Array,map,Set,argument등이 있다.
+//사용방법
+//for(변수 of 객체){ 반복적으로 실행하고자 하는 실행문; }
